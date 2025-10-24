27d84f37e5dea46be14ae641b29af72541b13c3d
Artin552 <pronin5524@gmail.com>
2025-10-24 22:41:35 +0300

    modified:   backend/routes/auth.js 	modified:   backend/users.db 	modified:   frontend/auth.html 	modified:   frontend/reg.html 	modified:   index.html

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -24,6 +24,8 @@ router.post('/register', (req, res) => {
         if (err) return res.status(500).json({ error: 'Ошибка при регистрации' });
         const user = { id: this.lastID, name, email };
         const token = jwt.sign(user, JWT_SECRET, { expiresIn: '7d' });
+
+
         res.json({ success: true, user, token, redirect: '/frontend/auth.html' });
       }
     );
@@ -33,15 +35,16 @@ router.post('/register', (req, res) => {
 // === Авторизация ===
 router.post('/login', (req, res) => {
   const { email, password } = req.body;
-  if (!email || !password) return res.status(400).json({ error: 'Email и пароль обязательны' });
 
   db.get('SELECT * FROM users WHERE email = ?', [email], async (err, user) => {
-    if (!user) return res.status(400).json({ error: 'Неверный email или пароль' });
-    const ok = await bcrypt.compare(password, user.password);
-    if (!ok) return res.status(400).json({ error: 'Неверный email или пароль' });
+    if (!user) return res.status(401).json({ success: false, error: 'Неверный логин или пароль' });
 
-    const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, { expiresIn: '7d' });
-    res.json({ user: { id: user.id, name: user.name, email: user.email }, token });
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) return res.status(401).json({ success: false, error: 'Неверный логин или пароль' });
+
+    const token = jwt.sign({ id: user.id, name: user.name, email: user.email }, JWT_SECRET, { expiresIn: '7d' });
+
+    res.json({ success: true, email: user.email, token, redirect: '/frontend/index.html' });
   });
 });
 
